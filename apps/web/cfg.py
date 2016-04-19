6753fe0b488ecd6482dec1e3c49165aa0f51a6c7
Mayorov Evgeny <motormen@gmail.com>
2016-04-19 16:40:53 +0300

    Added handlers and static to apps.web

--- apps/web/cfg.py
+++ apps/web/cfg.py
@@ -1,1 +1,23 @@
-from ikcms.apps.composite import Cfg
+import os
+
+from iktomi.utils import cached_property
+
+import ikcms.apps.composite
+
+
+class Cfg(ikcms.apps.composite.Cfg):
+
+    STATIC_ENABLED = True
+
+    @cached_property
+    def STATIC_DIR(self):
+        return os.path.join(self.SITE_DIR, 'static')
+
+    STATIC_URL = '/static/'
+
+    @cached_property
+    def MEDIA_DIR(self):
+        return os.path.join(self.ROOT_DIR, 'media')
+
+    MEDIA_URL = '/media/'
+
---
+++ apps/web/handler.py
@@ -0,0 +1,19 @@
+from ikcms.web import (
+    h_cases,
+    h_match,
+    h_static_files,
+    h_404,
+)
+
+
+def h_index(env, data):
+    return env.render('index.html')
+
+
+def get_handler(app):
+    return h_cases(
+        h_static_files(app.cfg.STATIC_DIR, app.cfg.STATIC_URL),
+        h_static_files(app.cfg.MEDIA_DIR, app.cfg.MEDIA_URL),
+        h_match('/', name='index') | h_index,
+        h_404,
+    )
