5a3f94017c2dd6e70d5d1cde8c064b5df95364a4
Mayorov Evgeny <motormen@gmail.com>
2016-07-06 17:22:19 +0300

    Added sync memcache and redis components

---
+++ components/cache/memcache.py
@@ -0,0 +1,34 @@
+import memcache
+from . import base
+
+
+class Component(base.Component):
+
+    DEFAULT_MEMCACHE_HOST = 'localhost'
+    DEFAULT_MEMCACHE_PORT = 11211
+    prefix = b''
+
+    def __init__(self, app, client):
+        super().__init__(app)
+        self.client = client
+
+    @classmethod
+    def create(cls, app):
+        host = getattr(app.cfg, 'MEMCACHE_HOST', cls.DEFAULT_MEMCACHE_HOST)
+        port = getattr(app.cfg, 'MEMCACHE_PORT', cls.DEFAULT_MEMCACHE_PORT)
+        return cls(app, client=memcache.Client([(host, port)]))
+
+    def get(self, key):
+        return self.client.get(self._key(key))
+
+    def set(self, key, value, expire=0):
+        return self.client.set(self._key(key), value, time=expire)
+
+    def delete(self, key):
+        return self.client.delete(self._key(key))
+
+    def _key(self, key):
+        return self.prefix + key
+
+
+component = Component.create_cls
---
+++ components/cache/redis.py
@@ -0,0 +1,30 @@
+import redis
+from . import base
+
+
+class Component(base.Component):
+
+    DEFAULT_REDIS_HOST = 'localhost'
+    DEFAULT_REDIS_PORT = 6379
+
+    def __init__(self, app, client):
+        super().__init__(app)
+        self.client = client
+
+    @classmethod
+    def create(cls, app):
+        host = getattr(app.cfg, 'REDIS_HOST', cls.DEFAULT_REDIS_HOST)
+        port = getattr(app.cfg, 'REDIS_PORT', cls.DEFAULT_REDIS_PORT)
+        return cls(app, client=redis.Redis(host=host, port=port))
+
+    def get(self, key):
+        return self.client.get(key)
+
+    def set(self, key, value, expire=0):
+        return self.client.set(key, value, ex=expire)
+
+    def delete(self, key):
+        return self.client.delete(key)
+
+
+component = Component.create_cls
