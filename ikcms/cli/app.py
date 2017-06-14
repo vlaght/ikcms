8dbfc8fe3f7ae7937580f921be2976848c052730
Mayorov Evgeny <motormen@gmail.com>
2017-06-14 15:36:14 +0300

    Added dev http server auto reloading

--- ikcms/cli/app.py
+++ ikcms/cli/app.py
@@ -12,16 +12,40 @@ class AppCli(Cli):
     name = 'app'
 
     def command_serve(self):
-        print('Staring HTTP server process...')
-        from wsgiref.simple_server import make_server
-        app = self.create_app()
 
-        server = make_server(
-            app.cfg.HTTP_SERVER_HOST,
-            app.cfg.HTTP_SERVER_PORT,
-            app,
-        )
-        server.serve_forever()
+        def http_process():
+            from wsgiref.simple_server import make_server
+            print('Staring HTTP server process...')
+            app = self.create_app()
+            server = make_server(
+                app.cfg.HTTP_SERVER_HOST,
+                app.cfg.HTTP_SERVER_PORT,
+                app,
+            )
+            server.serve_forever()
+
+        p1 = Process(target=http_process)
+        p1.start()
+
+        try:
+            wait_for_code_change()
+            p1.terminate()
+            p1.join()
+            flush_fds()
+
+            pid = os.fork()
+            if pid:
+                os.closerange(3, MAXFD)
+                os.waitpid(pid, 0)
+                os.execvp(sys.executable, [sys.executable] + sys.argv)
+            else:
+                sys.exit()
+
+        except KeyboardInterrupt:
+            print('Terminating HTTP and WS servers...')
+            p1.terminate()
+
+        sys.exit()
 
     def command_shell(self, level=None, cfg=''):
         kwargs = dict(custom_cfg_path=cfg)
