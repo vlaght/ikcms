ccfe5a16324f655a959e16b1f9058670ccd0bad4
Mayorov Evgeny <motormen@gmail.com>
2017-07-12 14:52:47 +0300

    Fixed admin env auth_model property

--- ikcms/apps/admin/env.py
+++ ikcms/apps/admin/env.py
@@ -62,20 +62,20 @@ class Environment(ikcms.apps.composite.env.Environment):
     def user(storage):
         return storage.user
 
-    @storage_cached_property
-    def models(storage):
+    @cached_property
+    def models(self):
         class Models(object):
-            admin = storage.app.db.get_models('admin')
-            front = storage.app.db.get_models('front')
+            admin = self.app.db.get_models('admin')
+            front = self.app.db.get_models('front')
         return Models()
 
     @storage_cached_property
     def redis(storage):
         return storage.app.cache.client
 
-    @storage_cached_property
-    def auth_model(storage):
-        return storage.models.AdminUser
+    @cached_property
+    def auth_model(self):
+        return self.models.admin.AdminUser
 
     @storage_cached_property
     def item_lock(storage):
