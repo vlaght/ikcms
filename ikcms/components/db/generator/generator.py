7dc08862a2693f9a305a0da2e3c1fb7691b1e4d6
Andrey Talantsev <slivtime@gmail.com>
2017-11-27 15:52:29 +0300

    Fix stringio import for py3

--- ikcms/components/db/generator/generator.py
+++ ikcms/components/db/generator/generator.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from StringIO import StringIO
+from io import StringIO
 from datetime import datetime, timedelta
 from random import randint, choice, sample, randrange, random
 
