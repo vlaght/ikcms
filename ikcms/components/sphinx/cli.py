d67da94ae66a78531cb804aa687e5dd3a55fb208
Vladislav Boyko <v1aght@ya.ru>
2020-02-25 12:52:46 +0300

    Refactored try-except block, better exception handling, removed shadowing of inner xmlpipe exceptions

--- ikcms/components/sphinx/cli.py
+++ ikcms/components/sphinx/cli.py
@@ -13,16 +13,20 @@ class SphinxCli(Cli):
         if not import_string:
             logging.info('Unknown xmlpipe "%s"', name)
             raise SystemExit
+        module_name, xmlpipe_name = import_string.rsplit('.', 1)
+
         try:
-            module_name, xmlpipe_name = import_string.rsplit('.', 1)
             module = import_module(module_name)
-            xmlpipe = getattr(module, xmlpipe_name)
-            xmlpipe(app)
         except ImportError as exc:
             logging.info('Could not load module "%s"', module_name)
             raise SystemExit
+
+        try:
+            xmlpipe = getattr(module, xmlpipe_name)
         except AttributeError as exc:
             logging.info('Could not find function "%s" in "%s"',
                          xmlpipe_name,
                          module_name)
             raise SystemExit
+
+        xmlpipe(app)
